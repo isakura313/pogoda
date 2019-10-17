1a734daa473b205e76776f56ff5e64c85f2dc358
Paul <isakura313@gmail.com>
2019-10-17 10:24:21 +0300

    all

--- code.js
+++ code.js
@@ -5,12 +5,18 @@
 
 let temperature = prompt('Температура за окном?');
 let temp=+temperature;  //перевод в число из строки
-let body = 
+let body = document.querySelector('body');
 
 if(temp>= -30 && temp<=-5){
     alert('холодно!');
+
+    body.classList.add('moroz')
 }else if(temp>= -5 && temp<=15){
     alert('тепло!');
+
+    body.classList.add('teplo')
 } else if(temp>= 15 && temp<=50){
     alert('жара');
+    
+    body.classList.add('hot')
 }
